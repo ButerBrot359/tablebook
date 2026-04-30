3ee1e3196a600641ddcf2d06fcc33ad06be91116
buterbrot359 <batowka359@gmail.com>
2026-04-30 18:32:26 +0500

    feat(auth): add register method in controller

--- backend/src/main/java/com/tablebook/auth/login/AuthController.java
+++ backend/src/main/java/com/tablebook/auth/login/AuthController.java
@@ -2,12 +2,11 @@ package com.tablebook.auth.login;
 
 import com.tablebook.auth.login.dto.LoginRequest;
 import com.tablebook.auth.login.dto.LoginResponse;
+import com.tablebook.auth.login.dto.RegisterRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/auth")
@@ -18,5 +17,11 @@ public class AuthController {
     @PostMapping("/login")
     public LoginResponse login(@Valid @RequestBody LoginRequest request) {
         return authService.login(request);
+    }
+
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.CREATED)
+    public LoginResponse register(@Valid @RequestBody RegisterRequest request) {
+        return authService.register(request);
     }
 }
